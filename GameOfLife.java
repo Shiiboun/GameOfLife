2a57e42ba1ad9a337106e4f91d10ecb52321d0d9
Thorben <Shiiboun@users.noreply.github.com>
2017-07-09 18:53:24 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -49,48 +49,179 @@ public class GameOfLife extends Application {
 		} // Ende while
 			// Checken ob es Nachbarn gibt
 
-		for (int i = 0; i < felder; i++) {
-			for (int j = 0; j < felder; j++) {
-				c[i][j] = b[i][j];
+		for (int spalte = 0; spalte < felder; spalte++) {
+			for (int reihe = 0; reihe < felder; reihe++) {
+				c[spalte][reihe] = b[spalte][reihe];
 				//Ränder auslassen
-				if ((i > 0) && (j > 0) && (i < felder - 1) && (j < felder - 1)) {
-					if (c[i - 1][j] == true) {
+				if ((spalte > 0) && (reihe > 0) && (spalte < felder - 1) && (reihe < felder - 1)) {
+					if (c[spalte - 1][reihe] == true) {
 						hatNachbarn++;
 					}
-					if (c[i - 1][j + 1] == true) {
+					else if (c[spalte - 1][reihe + 1] == true) {
 						hatNachbarn++;
 					}
-					if (c[i - 1][j - 1] == true) {
+					else if (c[spalte - 1][reihe - 1] == true) {
 						hatNachbarn++;
 					}
-					if (c[i + 1][j] == true) {
+					else if (c[spalte + 1][reihe] == true) {
 						hatNachbarn++;
 					}
-					if (c[i + 1][j + 1] == true) {
+					else if (c[spalte + 1][reihe + 1] == true) {
 						hatNachbarn++;
 					}
-					if (c[i + 1][j - 1] == true) {
+					else if (c[spalte + 1][reihe - 1] == true) {
 						hatNachbarn++;
 					}
-					if (c[i][j - 1] == true) {
+					else if (c[spalte][reihe - 1] == true) {
 						hatNachbarn++;
 					}
-					if (c[i][j + 1] == true) {
+					else if (c[spalte][reihe + 1] == true) {
 						hatNachbarn++;
 					}
 				}
+				//Linker Rand
+				else if(spalte == 0){
+					//oben links
+					if (reihe == 0){
+						if (c[spalte + 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+					}
+					//unten links
+					else if (reihe == felder-1){
+						if (c[spalte][reihe -1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe] == true) {
+							hatNachbarn++;
+						}
+					}
+					//dazwischen
+					else if ((reihe > 0) && (reihe < felder -2)){
+						if (c[spalte][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe- 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+					}
+				}
+				//Rechter Rand
+				else if (spalte == felder-1){
+					//Oben rechts
+					if (reihe == 0){
+						if (c[spalte - 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+					}
+					//unten rechts
+					else if (reihe == felder -1){
+						if (c[spalte - 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+					}
+					//dazwischen
+					else if ((reihe > 0) && (reihe < felder -2)){
+						if (c[spalte][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe- 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+					}
+				}
+				//Oberer Rand
+				else if (reihe == 0){
+					if ((spalte > 0) && (spalte < felder - 1)){
+						if (c[spalte][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte  + 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe + 1] == true) {
+							hatNachbarn++;
+						}
+					}
+				}
+				//Unterer Rand
+				else if (reihe == felder -1){
+					if ((spalte > 0) && (spalte < felder - 1)){
+						if (c[spalte][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte  + 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte - 1][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+						else if (c[spalte + 1][reihe - 1] == true) {
+							hatNachbarn++;
+						}
+					}
+				}
+					
 				// Überprüfen ob tot
 				if ((hatNachbarn > 3) || (hatNachbarn < 2)) {
-					System.out.println(i + " " + j + "  ist tot");
+					System.out.println(spalte + " " + reihe + "  ist tot");
 					// r[i][j].setFill(Color.BEIGE);
 				}
 				// Überprüfen ob überlebt
-				if ((hatNachbarn == 2) || (hatNachbarn == 3)) {
-					System.out.println(i + " " + j + " hat überlebt");
+				else if ((hatNachbarn == 2) || (hatNachbarn == 3)) {
+					System.out.println(spalte + " " + reihe + " hat überlebt");
 				}
 				// Überprüfen ob geburt
-				if (hatNachbarn == 3) {
-					System.out.println(i + " " + j + "   wurde geboren");
+				else if (hatNachbarn == 3) {
+					System.out.println(spalte + " " + reihe + "   wurde geboren");
 					// r[i][j].setFill(Color.BLUE);
 				}
 			}
@@ -98,7 +229,7 @@ public class GameOfLife extends Application {
 		st.setScene(sz);
 		st.show();
 
-		int count = 0;
+		/*int count = 0;
 		for (int i = 0; i < felder; i++) {
 			for (int j = 0; j < felder; j++) {
 				System.out.println("Checktrue " + i + " " + j + " " + b[i][j]);
@@ -107,7 +238,7 @@ public class GameOfLife extends Application {
 				}
 			}
 		}
-		System.out.println(count);
+		System.out.println(count);*/
 
 	}
 }
